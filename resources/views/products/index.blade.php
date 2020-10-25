87dc03c803080236f41f1aa7f67ceb89736cba3d
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-10-24 22:36:54 -0600

    Agrega el CSS de Bootstrap a la vista
    Agrega clases de CSS a la tabla de productos

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,31 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Productos</h1>
-<table>
-    <thead>
-        <tr>
-            <th>Id</th>
-            <th>Nombre</th>
-            <th>Descripcion</th>
-            <th>Precio</th>
-            <th>Imagen</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($products as $product)
-        <tr>
-            <td>{{ $product->id }}</td>
-            <td>{{ $product->name }}</td>
-            <td>
-                <p>{{ $product->description }}</p>
-            </td>
-            <td>$ {{ $product->price }}</td>
-            <td>
-                <img src="{{ $product->image }}" height="100" />
-            </td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
+<div class="row">
+    <div class="col-12">
+        <h1>Productos</h1>
+    </div>
+    <div class="col-12">
+        <table class="table table-bordered table-responsive">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Nombre</th>
+                    <th>Descripcion</th>
+                    <th>Precio</th>
+                    <th>Imagen</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($products as $product)
+                <tr>
+                    <td>{{ $product->id }}</td>
+                    <td>{{ $product->name }}</td>
+                    <td>
+                        <p>{{ $product->description }}</p>
+                    </td>
+                    <td>$ {{ $product->price }}</td>
+                    <td>
+                        <img src="{{ $product->image }}" height="100" />
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
 @endsection
